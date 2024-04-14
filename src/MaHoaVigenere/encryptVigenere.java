683537814c0b4f350bcfb6460b215fa7028d363a
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-04-14 18:20:46 +0700

    :memo: update code

--- src/MaHoaVigenere/encryptVigenere.java
+++ src/MaHoaVigenere/encryptVigenere.java
@@ -4,10 +4,40 @@
  */
 package MaHoaVigenere;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ADMIN
  */
 public class encryptVigenere {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ban ro: ");
+        String plainText = sc.nextLine();
+        System.out.print("Khoa vigenere: ");
+        String key = sc.nextLine();
+
+        String encryptedTexT = encrypt(plainText, key);
+        System.out.println("Ban ma: " + encryptedTexT);
+    }
+
+    private static String encrypt(String plainText, String key) {
+        StringBuilder result = new StringBuilder();
+        int keyIndex = 0;
+        for (int i = 0; i < plainText.length(); i++) {
+            char ch = plainText.charAt(i);
+            if (Character.isLetter(ch)) {
+                char base = Character.isUpperCase(ch) ? 'A' : 'a';
+                char encryptedChar = (char) ((ch + key.charAt(keyIndex) - 2 * base) % 26 + base);
+                result.append(encryptedChar);
+                keyIndex = (keyIndex + 1) % key.length();
+            } else {
+                result.append(ch);
+            }
+        }
+
+        return result.toString();
+    }
 }
